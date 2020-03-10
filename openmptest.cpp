19689a5e80a3ec44d79dc6d92d1f527d7702a0aa
Defense <junhun01@hotmail.com>
2020-03-10 15:03:43 +0800

    opmp

---
+++ openmptest.cpp
@@ -0,0 +1,29 @@
+﻿#include <omp.h>
+#include <iostream>
+using namespace std;
+static long num_steps = 1000000;
+double step;
+#define NUM_THREADS 2 
+void main()
+{
+	int i[NUM_THREADS];
+	double x, pi, sum[NUM_THREADS];
+	step = 1.0 / (double)num_steps;
+	omp_set_num_threads(NUM_THREADS); //
+#pragma omp parallel
+	{
+		double x;
+		int id;
+		id = omp_get_thread_num();
+		for (i[id] = id, sum[id] = 0.0; i[id] < num_steps; i[id] = i[id] + NUM_THREADS) {//
+			x = (i[id] + 0.5) * step;
+			//cout << "i " << i[id] << " id" << id << endl;
+			sum[id] += 4.0 / (1.0 + x * x);
+		}
+	} 
+	pi = 0.0;
+	for (int i = 0; i < NUM_THREADS; i++)
+		pi += sum[i] * step;
+	cout << "PI:" << pi << endl;
+	system("pause");
+}
